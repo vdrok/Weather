05593c1d78f74059698e48c3d3fb755d881f5b22
websharp <websharp@websharps-iMac.local>
2019-03-07 15:38:22 -0500

    use only weather-js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import weather from "weather-js"
-import zipcodes from "zipcodes"
+// import zipcodes from "zipcodes"
 
 const points = [
 	{ name: "AL", code: 35801 },
@@ -9,9 +9,9 @@ const points = [
 ]
 
 points.map((point) => {
-	const location = zipcodes.lookup(point.code)
+	// const location = zipcodes.lookup(point.code)
 	// tslint:disable-next-line
-	console.log("location: ", location)
+	// console.log("location: ", location)
 	weather.find({search: point.code, degreeType: "F"}, (err: any, result: any)  => {
 		// tslint:disable-next-line
 		if(err) console.log("Error: ", err)
